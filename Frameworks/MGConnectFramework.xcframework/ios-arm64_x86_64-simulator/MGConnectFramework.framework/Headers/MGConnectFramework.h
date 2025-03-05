489693c15695ed80ec9a2395e8f44cee0cf622f2
Shimon Shnitzer <contact@shsh.com>
2025-03-05 19:03:44 +0200

    Initial release

---
+++ Frameworks/MGConnectFramework.xcframework/ios-arm64_x86_64-simulator/MGConnectFramework.framework/Headers/MGConnectFramework.h
@@ -0,0 +1,20 @@
+//
+//  MGConnectFramework.h
+//  MGConnectFramework
+//
+//  Created by Shimon Shnitzer on 05/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//#import <BLEInterface/BLEInterface.h>
+
+//! Project version number for MGConnectFramework.
+FOUNDATION_EXPORT double MGConnectFrameworkVersionNumber;
+
+//! Project version string for MGConnectFramework.
+FOUNDATION_EXPORT const unsigned char MGConnectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGConnectFramework/PublicHeader.h>
+
+
